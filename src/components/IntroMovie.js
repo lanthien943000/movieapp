c6a3f140e873f1ff916f4f384346341015dfee54
lanthien943000 <hongnhat24022000@gmail.com>
2022-09-18 03:56:26 +0700

    fix IntroMovie

--- src/components/IntroMovie.js
+++ src/components/IntroMovie.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import { useState } from "react";
 
 function IntroMovie() {
+  const [isMuted, setIsMuted] = useState(true);
   return (
     <div className="introl_container">
       <ReactPlayer
@@ -12,7 +16,15 @@ function IntroMovie() {
         height="100%"
         volume={0.3}
         className="videoItrol"
+        muted={isMuted}
       />
+      <div onClick={() => setIsMuted((e) => !e)} className="vollume">
+        {isMuted ? (
+          <VolumeOffIcon id="btnVolume" sx={{ fontSize: "40px" }} />
+        ) : (
+          <VolumeUpIcon id="btnVolume" sx={{ fontSize: "40px" }} />
+        )}
+      </div>
       <div className="fadebottom"></div>
     </div>
   );
